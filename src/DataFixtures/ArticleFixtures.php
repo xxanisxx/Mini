5fa64103e20546f86e1f3d7f4c1d8d6e1b1ba881
xxanisxx <aniscaca3@gmail.com>
2019-05-11 18:57:31 -0700

    first commit

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -7,12 +7,13 @@ use App\Entity\Comment;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $faker = \Faker\Factory::create('fr_FR');
+        $faker = Faker\Factory::create('fr_FR');
         for ($i=1; $i <=4 ; $i++) { 
             $category = new Category();
             $category->setTitle($faker->sentence())
